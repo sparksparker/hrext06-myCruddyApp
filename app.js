78cfdef795f26b0846a1e60862617c3474fe4a4e
sparks <sparks@sparkss-MacBook-Pro.local>
2018-12-18 01:39:03 -0800

    feat(adds multiple fields in an object, but by localStorage.setItem 3x):

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 $(document).ready(function(){
   console.log('jQuery loaded');
 
+  var dataArr = [];
+
   var $btnSubmit = $('.btn-submit');
   var $btnDelete = $('.btn-delete');
+  var $textFirstName = $('.text-firstname');
+  var $textLastName = $('.text-lastname');
+  var $textEmail = $('.text-email');
+
+  // store multiple values
+  $btnSubmit.on('click', function() {
+    if('localStorage' in window && window['localStorage'] !== null) {
+        var user = {
+          'firstname': $textFirstName.val(),
+          'lastname': $textLastName.val(),
+          'email': $textEmail.val(),
+        }
+        var record = JSON.stringify(user);
+        localStorage.setItem(user.firstname, record);
+        localStorage.setItem(user.lastname, record);
+        localStorage.setItem(user.email, record);
+    }
+  });
 
   // write to local storage from input when button save clicked
-  $btnSubmit.on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
-    console.log('myItemInStorage', myItemInStorage);
-
-    // display the value here
-    $('.list-display-field').text(myItemInStorage); // ??
-
-  });
+  // $btnSubmit.on('click', function(){
+  //   localStorage.setItem('inputFieldValue', $('.text-entry').val());
+  //   var myItemInStorage = localStorage.getItem('inputFieldValue');
+  //   console.log('myItemInStorage', myItemInStorage);
+  //
+  //   // display the value here
+  //   $('.list-display-field').text(myItemInStorage); // ??
+  //
+  // });
 
   // delete from local storage when delete button clicked
   $btnDelete.on('click', function(){
