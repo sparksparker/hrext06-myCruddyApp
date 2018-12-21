c91b60fdbf9ff8b575f0ab515d3eaea0816ff4ec
sparks <sparks@sparkss-MacBook-Pro.local>
2018-12-20 21:29:55 -0800

    feat(shows survey results, button to make changes, and another button to start over):

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ $(document).ready(function(){
   var $displayField = $('.display-field');
   var $resultsDisplayField = $('.results-display-field');
   var $resultsPage = $('.results-page');
+  var $surveyResults = $('.survey-results');
+  var $btnRestartSurvey = $('.btn-restart-survey');
+  var $btnNotYou = $('.btn-not-you');
 
 
   // setting global variable for JSON.stringify
@@ -60,10 +63,28 @@ $(document).ready(function(){
     $resultsPage.toggle();
 
     $resultsDisplayField.text(`Great news, ${$textFirstName.val()}! We found some matches for you.`)
+
+    var department = $('#department').val();
+    var features = $('#features').val();
+    var employees = $('#employees').val();
+
+    $surveyResults.text(`You selected:
+      Department: ${department}
+      Features: ${features}
+      Employees: ${employees}`);
+
   });
 
+  $btnRestartSurvey.on('click', function() {
+    $resultsPage.toggle();
+    $previewPage.toggle();
+  })
 
-
+  $btnNotYou.on('click', function() {
+    $previewPage.toggle();
+    $landingPage.toggle();
+    location.reload();
+  })
 
   // $btnDeleteAll.on('click', function() {
   //   localStorage.clear();
